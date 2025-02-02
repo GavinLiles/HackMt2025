0190f43dd6a181484c622677495a568492cc5258
Jake Weaver <jakew1380@yahoo.com>
2025-02-02 01:49:52 -0600

    Level 1 Final v1

---
+++ scripts/hall.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+var friendly_count = 0
+var enemy_count = 0
+Global.HALLWAY
+Global.room_timers[HALLWAY]
+var timing = false
+
+func _physics_process(delta):
+# Get all bodies currently overlapping the detection area
+	var overlapping_bodies = get_overlapping_bodies()
+# Count friendlies and enemies
+	friendly_count = 0
+	enemy_count = 0
+
+	for body in overlapping_bodies:
+		if body.is_in_group("friendly"):
+			friendly_count += 1
+		elif body.is_in_group("enemy"):
+			enemy_count += 1
+
+# Display counts in the output console
+	#print("Friendlies in area:", friendly_count)
+	#print("Enemies in area:", enemy_count)
+
+func _process(delta: float) -> void:
+	if friendly_count > 0 and enemy_count > 0 and !timing:
+		$Timer.start()
+		timing = true
+
+func _on_timer_timeout() -> void:
+	Global.room_timers[HALLWAY] += 1
+	if friendly_count > 0 and enemy_count > 0:
+		$Timer.start()
+	else:
+		timing = false
+		Global.room_timers[HALLWAY] = 0
