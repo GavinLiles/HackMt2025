eb9e88aae7ef9d8cf6dec3fa126f0cba68e25495
kenjim06 <kenjilyamrichardson@gmail.com>
2025-02-02 02:08:50 +0000

    Deploying to gh-pages from @ GavinLiles/HackMt2025@bced277ff980d19716d31ad341ee614a6c7ee2f6 🚀

--- scene/patrol.gd
+++ scene/patrol.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
+
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func patrol():
-	var direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
-	
+func _process(delta: float) -> void:
+	pass
